d3224a70c76efbea4397d7282b35e7f82bec1bc9
makamys <makamys@outlook.com>
2023-04-21 19:48:44 +0200

    Set extraParamsCsvs

--- src/main/java/io/legacymoddingmc/mappinggenerator/MappingGeneratorPlugin.java
+++ src/main/java/io/legacymoddingmc/mappinggenerator/MappingGeneratorPlugin.java
@@ -3,8 +3,11 @@
  */
 package io.legacymoddingmc.mappinggenerator;
 
+import com.gtnewhorizons.retrofuturagradle.IMinecraftyExtension;
 import org.gradle.api.Project;
 import org.gradle.api.Plugin;
+
+import java.io.File;
 
 /**
  * A simple 'hello world' plugin.
@@ -15,5 +18,9 @@ public class MappingGeneratorPlugin implements Plugin<Project> {
         project.getTasks().register("greeting", task -> {
             task.doLast(s -> System.out.println("Hello from plugin 'io.legacymoddingmc.mappinggenerator.greeting'"));
         });
+
+        IMinecraftyExtension minecraft = (IMinecraftyExtension)project.getExtensions().getByName("minecraft");
+        System.out.println("Setting the thing !");
+        minecraft.getExtraParamsCsvs().from(new File(project.getBuildDir(), "extra-mappings/test.csv"));
     }
 }

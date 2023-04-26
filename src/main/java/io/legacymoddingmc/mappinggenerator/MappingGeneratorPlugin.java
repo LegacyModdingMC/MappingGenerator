ae108997e9d4395ede871d5fe1895c7f394bdb9f
makamys <makamys@outlook.com>
2023-04-26 07:49:26 +0200

    We do a little output jar deletion

--- src/main/java/io/legacymoddingmc/mappinggenerator/MappingGeneratorPlugin.java
+++ src/main/java/io/legacymoddingmc/mappinggenerator/MappingGeneratorPlugin.java
@@ -4,6 +4,7 @@
 package io.legacymoddingmc.mappinggenerator;
 
 import com.gtnewhorizons.retrofuturagradle.IMinecraftyExtension;
+import com.gtnewhorizons.retrofuturagradle.mcp.RemapSourceJarTask;
 import io.legacymoddingmc.mappinggenerator.source.MCPSource;
 import io.legacymoddingmc.mappinggenerator.source.YarnSource;
 import org.gradle.api.Project;
@@ -11,7 +12,6 @@ import org.gradle.api.Plugin;
 import org.gradle.api.tasks.TaskProvider;
 
 import java.io.File;
-import java.util.Arrays;
 
 /**
  * A simple 'hello world' plugin.
@@ -20,7 +20,25 @@ public class MappingGeneratorPlugin implements Plugin<Project> {
     public void apply(Project project) {
         File outFile = new File(project.getBuildDir(), "extra-mappings/parameters.csv");
 
-        // Register a task
+        TaskProvider<?> taskPreGenerateExtraMappings = project.getTasks().register("preGenerateExtraMappings", task -> {
+            task.doLast(s -> {
+                boolean doIWantToRun = true;
+                if(doIWantToRun) {
+                    RemapSourceJarTask taskRemapDecompiledJar = (RemapSourceJarTask)project.getTasks().getByName("remapDecompiledJar");
+                    File remappedJar = taskRemapDecompiledJar.getOutputJar().get().getAsFile();
+                    if(remappedJar.isFile()) {
+                        System.out.println("Deleting " + remappedJar.getName() + " to force the decompilation chain to re-run!");
+                        remappedJar.delete();
+                    }
+                } else {
+                    project.getTasks().getByName("generateExtraMappings").setEnabled(false);
+                }
+            });
+        });
+        project.getTasks().getByName("mergeVanillaSidedJars").dependsOn(taskPreGenerateExtraMappings);
+        // We want to analyze the Forge-patched Minecraft jar, but it only exists while RFG's deobfuscation task chain
+        // is running! So, we must force RFG to run decompilation in order to access it.
+
         TaskProvider<?> taskGenerateExtraMappings = project.getTasks().register("generateExtraMappings", task -> {
             task.doLast(s -> {
                 System.out.println("Running generateExtraMappings!");

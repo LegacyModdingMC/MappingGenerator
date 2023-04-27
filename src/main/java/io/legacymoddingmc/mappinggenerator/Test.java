4ad344214cdbc217a3b1864f99be3ef0f677ba2a
makamys <makamys@outlook.com>
2023-04-27 13:55:36 +0200

    Parsing test with java compiler api. It's so fast!

--- src/main/java/io/legacymoddingmc/mappinggenerator/Test.java
+++ src/main/java/io/legacymoddingmc/mappinggenerator/Test.java
@@ -1,21 +1,19 @@
 package io.legacymoddingmc.mappinggenerator;
 
 import com.gtnewhorizons.retrofuturagradle.shadow.com.google.common.collect.Lists;
-import com.gtnewhorizons.retrofuturagradle.shadow.org.apache.commons.io.IOUtils;
-import com.sun.source.tree.ClassTree;
-import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.Tree;
+import com.sun.source.tree.*;
 import com.sun.source.util.JavacTask;
+import com.sun.source.util.TreePath;
+import com.sun.source.util.TreePathScanner;
 import lombok.SneakyThrows;
 import lombok.val;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.tools.JavaCompiler;
@@ -50,24 +48,64 @@ public class Test {
         val parsed = Lists.newArrayList(((JavacTask) task).parse());
 
         for(CompilationUnitTree cu : parsed) {
-            for(Tree tree : cu.getTypeDecls()) {
-                if(tree instanceof ClassTree) {
-                    ClassTree classTree = (ClassTree)tree;
-                    System.out.println(classTree.getSimpleName());
-                    for(Tree memberTree : classTree.getMembers()) {
-                        if(memberTree instanceof MethodTree) {
-                            MethodTree method = (MethodTree)memberTree;
-                            System.out.println("  " + method.getName());
-                        }
-                    }
-                }
-            }
+            System.out.println("CU: " + cu.getSourceFile().getName());
+            MyVisitor visitor = new MyVisitor();
+            visitor.scan(cu, null);
             System.out.println();
         }
 
         long t1 = System.nanoTime();
 
         System.out.println("Parsed " + 0 + " classes in " + (t1-t0) / 1_000_000_000.0 + " seconds.");
+    }
+
+    public static class MyVisitor extends TreePathScanner<Void, Void> {
+
+        @Override
+        public Void visitVariable(VariableTree variableTree, Void unused) {
+            TreePath path = getCurrentPath();
+            String msg = "";
+
+            MethodTree method = null;
+            int methodIndex = -1;
+            BlockTree block = null;
+            int blockIndex = -1;
+            String className = "";
+            CompilationUnitTree cu = null;
+
+            int i = 0;
+            while(path != null) {
+                Tree leaf = path.getLeaf();
+
+                if(method == null && leaf instanceof MethodTree) {
+                    method = (MethodTree) leaf;
+                    methodIndex = i;
+                } else if(block == null && leaf instanceof BlockTree) {
+                    block = (BlockTree) leaf;
+                    blockIndex = i;
+                }
+                if(leaf instanceof ClassTree) {
+                    ClassTree klass = (ClassTree) leaf;
+                    String name = klass.getSimpleName().toString();
+                    if(className.isEmpty()) {
+                        className = name;
+                    } else {
+                        className = name + "$" + className;
+                    }
+                }
+                if(leaf instanceof CompilationUnitTree) {
+                    cu = (CompilationUnitTree) leaf;
+                }
+
+                i++;
+                path = path.getParentPath();
+            }
+
+            if(method != null && block != null && blockIndex < methodIndex) {
+                System.out.println(cu.getPackageName().toString() + "." + className + " " + method.getName() + "(" + String.join(", ", method.getParameters().stream().map(v -> v.getType().toString() + " " + v.getName().toString()).collect(Collectors.toList())) + ") : " + variableTree.getType() + " " + variableTree.getName().toString());
+            }
+            return super.visitVariable(variableTree, unused);
+        }
     }
 
 }
